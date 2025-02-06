a3f15731f081532dd2dde62a2d9f5cfb07561db2
dgates888 <105668947+dgates888@users.noreply.github.com>
2025-02-06 12:16:18 -0600

    Add files via upload

---
+++ topic1-1/src/main/java/com/David/Topic11Application.java
@@ -0,0 +1,15 @@
+package com.David;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Topic11Application.class, args);
+		System.out.println("hello world");
+		
+	}
+
+}
---
+++ topic1-1/src/test/java/com/David/Topic11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.David;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
